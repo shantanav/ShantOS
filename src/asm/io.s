d1ce9fb2ed65600c6ed5b1c3c55abffca7f11c68
Shantanav Saurav <ss9415@rit.edu>
2021-09-22 18:49:39 -0400

    Moved gdt loading to its own file

--- src/asm/io.s
+++ src/asm/io.s
@@ -18,11 +18,3 @@ outb:
     out dx, al              ; send the data to the I/O port
     ret                     ; return to the calling function
 
-global load_gdt
-; load_gdt - Loads the global descriptor table and local descriptor tables
-; stack:    [esp + 4] Address to a GDT structure
-;           [esp    ] return address
-load_gdt:
-    lgdt [esp + 4]              ; Load GDT table from ESP register
-    ret
-
