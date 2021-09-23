2ac677212845dd66998c199fe3cdb8a715a68a80
Shantanav Saurav <ss9415@rit.edu>
2021-09-23 16:26:48 -0400

    Removed all references to GDTs
    Turns out that x86 runs in protected mode by
    default, therefore GDTs are not necessary,
    apparently. Today I learned...

--- src/asm/gdt.s
+++
@@ -1,8 +0,0 @@
-global load_gdt
-; load_gdt - Loads the global descriptor table and local descriptor tables
-; stack:    [esp + 4] Address to a GDT structure
-;           [esp    ] return address
-load_gdt:
-    lgdt [esp + 4]              ; Load GDT table from ESP register
-    ret
-
--- src/tables.c
+++
@@ -1,9 +0,0 @@
-#include "drivers.h"
-
-void create_tables() {
-    gdt_t gdt;
-    gdt.address = 0x800;
-    gdt.size = 65535;
-
-    load_gdt(&gdt);
-}
